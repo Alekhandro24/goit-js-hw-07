528e7ce89551fab8c5df240a563d6aea7d11b011
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2022-09-18 17:08:18 +0300

    add 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,48 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const items = [];
+
+galleryItems.forEach(element => {
+  const galleryItem = document.createElement('div');
+  galleryItem.classList.add('gallery__item');
+
+  const galleryLink = document.createElement('a');
+  galleryLink.classList.add('gallery__link');
+  galleryLink.href = element.original;
+
+  const galleryImg = document.createElement('img');
+  galleryImg.classList.add('gallery__image');
+  galleryImg.src = element.preview;
+  galleryImg.setAttribute('data-source', element.original);
+  galleryImg.alt = element.description;
+
+  galleryItem.append(galleryLink);
+  galleryLink.append(galleryImg);
+  items.push(galleryItem);
+});
+
+gallery.append(...items);
+
+gallery.addEventListener('click', e => {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const selectedImage = e.target.getAttribute('data-source');
+
+  const instance = basicLightbox.create(`
+    <img src="${selectedImage}" width="800" height="600">
+`);
+
+  instance.show();
+
+  gallery.addEventListener('keydown', e => {
+    if (e.key === 'Escape') {
+      instance.close();
+    }
+  });
+});
